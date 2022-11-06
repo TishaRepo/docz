260a3b5490e67d96250a0dee5fd1d7b29ee9e3f0
Tisha Madame <tishamadame29@gmail.com>
2022-11-06 23:06:00 +0530

    icon added

--- core/gatsby-theme-docz/src/components/Icons/index.js
+++ core/gatsby-theme-docz/src/components/Icons/index.js
@@ -8,4 +8,3 @@ export { default as Menu } from 'react-feather/dist/icons/menu'
 export { default as Search } from 'react-feather/dist/icons/search'
 export { default as Sun } from 'react-feather/dist/icons/sun'
 export {default as HowDoI} from 'react-feather/dist/icons/help-circle'
-
