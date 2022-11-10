7c86657ce3abb5760fa46808287da9e69408895a
Tisha Madame <112636486+TishaRepo@users.noreply.github.com>
2022-11-10 13:18:36 +0530

    testing done

---
+++ cypress/e2e/changelog.cy.js
@@ -0,0 +1,16 @@
+context('Window', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/changelog')
+    })
+  
+    it('cy.window() - get the global window object', () => {
+      
+      cy.window().should('have.property', 'top')
+    })
+  
+    it('cy.document() - get the document object', () => {
+     
+      cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
+    })
+})
+
